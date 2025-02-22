b8fa72c78d0e5c5b5a795763dc525878278f2e54
wlan418 <wlan@live.unc.edu>
2025-02-21 23:50:07 -0500

    created backend

---
+++ P1ReimbursementBackend/src/main/java/com/revature/P1ReimbursementBackend/P1ReimbursementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.revature.P1ReimbursementBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P1ReimbursementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P1ReimbursementBackendApplication.class, args);
+	}
+
+}
---
+++ P1ReimbursementBackend/src/test/java/com/revature/P1ReimbursementBackend/P1ReimbursementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.P1ReimbursementBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P1ReimbursementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
